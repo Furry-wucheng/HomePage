33c9ad16ec637607a31f556a0dffad008b529952
lejunl <2538000780@qq.com>
2026-02-10 15:02:01 +0800

    feat(config): 支持从YAML文件配置友链数据

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@
 import svelte from '@astrojs/svelte';
 import { defineConfig } from 'astro/config';
 import icon from 'astro-icon';
+import yaml from '@rollup/plugin-yaml';
 
 import tailwindcss from '@tailwindcss/vite';
 
@@ -12,6 +13,6 @@ export default defineConfig({
   integrations: [svelte(), icon()],
 
   vite: {
-    plugins: [tailwindcss()],
+    plugins: [tailwindcss(), yaml()],
   },
 });
